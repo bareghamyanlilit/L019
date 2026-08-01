1bc5b30f36fb301aa8447580326fedce8a1dc209
Lilit <baregamyanlilit36@gmail.com>
2026-08-01 22:43:28 +0400

    .

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,7 +10,7 @@ export const metadata: Metadata = {
     siteName: "Արամ և Սաթեն",
     images: [
       {
-        url: "https://l019.vercel.app/img2.png",
+        url: "https://l019.vercel.app/img1.png",
         width: 1200,
         height: 630,
       },
